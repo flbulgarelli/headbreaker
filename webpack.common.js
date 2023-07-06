cb0e36b97d8d00e998cb16052211f08baa8ad56f
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-07-06 14:02:03 -0300

    Integrate ts into webpack

--- webpack.common.js
+++ webpack.common.js
@@ -3,13 +3,23 @@ const path = require('path');
 const buildFolder = 'dist';
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    main: './index.js'
-  },
+//  context: path.resolve(__dirname, 'src'),
+  entry: './src/index.ts',
   output: {
     filename: 'headbreaker.js',
     path: path.resolve(__dirname, buildFolder),
     library: 'headbreaker'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
