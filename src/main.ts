045e8def1c6ccf90e8e4af9bc5a19856189360f2
Alexander <alexxycarlsson@gmail.com>
2024-09-14 13:13:40 +0200

    Update the testing demo

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import './style.css';
 import * as headbreaker from '@/headbreaker';
 
-document.querySelector<HTMLDivElement>(
-  '#app'
-)!.innerHTML = `<div id="puzzle"></div>`;
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<h1>Headbreaker Puzzle Demo</h1>
+<div id="puzzle"></div>
+`;
 
 window.headbreaker = headbreaker;
 
 const autogen = new headbreaker.Canvas('puzzle', {
   width: 800,
-  height: 650,
+  height: 400,
   pieceSize: 100,
   proximity: 20,
   borderFill: 10,
