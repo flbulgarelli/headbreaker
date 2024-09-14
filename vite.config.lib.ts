5dabbd3aafb30c2bfb14cec0dcd4e88fc12b86a4
Alexander <alexxycarlsson@gmail.com>
2024-09-14 13:20:36 +0200

    Separate demo website and library

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -3,9 +3,6 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: [
       {
---
+++ vite.config.website.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
+});
